c01cbf99c3228416f40d5cdf02428845def07066
evan2s2 <m1805240@edu.misis.ru>
2024-05-06 12:16:26 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+
+    <img alt="Vue logo" src="./assets/logo.png">
+
+    <p>{{ inputData }}</p>
+    <input type="text" v-model="inputData" />
+    
+    <button @click="onAddInputDatatoList">Click the button</button>
+
+    <ul>
+      <li v-for="elem in inputDataArray" :key="elem" >{{elem}}</li>
+    </ul>
+  
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
+
   name: 'App',
+
+  data(){
+    return {
+      inputData:null,
+      inputDataArray:[]
+    };
+  },
+
+  methods:{
+    /* *This method adds input data to out array* */
+    onAddInputDatatoList(){
+      this.inputDataArray.push(this.inputData);
+      console.log("the list is", this.inputDataArray)
+    }
+  },
+
   components: {
-    HelloWorld
   }
 }
+
 </script>
 
 <style>
